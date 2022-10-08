a4fbc6b7a4ee177935b27217779535bf89f6e0b7
dev-back55 <ahabitu@gmail.com>
2022-10-08 16:26:14 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-        Hi, I'm <a href="https://horacio-abitu.vercel.app">Horacio!</a>
+        Hi, I am <a href="https://horacio-abitu.vercel.app">Horacio!</a>
         </h1>
 
         <p className={styles.description}>
